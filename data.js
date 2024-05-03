5d807acaa6b06cd8c86908a9c586cd73adc17f72
aerogeo1 <aerogeo1.kvant@gmail.com>
2024-05-03 16:42:50 +0500

    Load Marzipano Project

---
+++ data.js
@@ -0,0 +1,168 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pxl_20240503_104336549photosphere",
+      "name": "PXL_20240503_104336549.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.0290322873773192,
+        "pitch": 0.19436604181606398,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.39045947461525543,
+          "pitch": 0.15676449262467074,
+          "rotation": 0,
+          "target": "1-pxl_20240502_164452854photosphere"
+        },
+        {
+          "yaw": -2.9561789495727417,
+          "pitch": 0.16809790586205509,
+          "rotation": 0,
+          "target": "2-20240424_125907---20240424_132220"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7751365937510855,
+          "pitch": 0.3637863465168252,
+          "title": "Некий цветок",
+          "text": "Вырабатывает кислород"
+        }
+      ]
+    },
+    {
+      "id": "1-pxl_20240502_164452854photosphere",
+      "name": "PXL_20240502_164452854.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.4783103071540644,
+        "pitch": 0.17443181264060215,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9906136626535105,
+          "pitch": 0.2470934203739823,
+          "rotation": 0,
+          "target": "0-pxl_20240503_104336549photosphere"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8120577313782569,
+          "pitch": 0.09538360478060426,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.4437042512162348,
+          "pitch": -0.10703193315394088,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.39598556483852043,
+          "pitch": 0.682907333736992,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-20240424_125907---20240424_132220",
+      "name": "20240424_125907 - 20240424_132220",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.693221781312758,
+          "pitch": 0.023947596118485848,
+          "rotation": 0,
+          "target": "0-pxl_20240503_104336549photosphere"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7802164441527459,
+          "pitch": -0.0002632985483295158,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
